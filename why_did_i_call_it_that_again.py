40fc3ca87f58f6d6f2e06c278c2fea4f2860c246
rethinkinggarhostlocation2202 <rethinkinggarhost@gmail.com>
2026-02-07 15:48:22 +0000

    Update and rename why_did_i_call_it_that_again.c to why_did_i_call_it_that_again.py

---
+++ why_did_i_call_it_that_again.py
@@ -0,0 +1,15 @@
+def step_1(arg: int):
+    key = ""
+    if arg == 3:
+        key = "spEed"
+    else:
+        #huh i can't type out the key could someone help me
+        #what i'm trying to get is that horse one
+        key = "[REDACTED]"
+
+def step_5_attack(input: str):
+    #i actually don't know how to encode a vigenere cipher
+    #can someone else get on ts
+
+    #for now just returning nothing
+    return ""
