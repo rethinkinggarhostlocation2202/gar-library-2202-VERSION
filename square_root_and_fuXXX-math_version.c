029110535726696096cf779dfc0f40360c1cfb5c
rethinkinggarhostlocation2202 <rethinkinggarhost@gmail.com>
2026-02-07 15:34:25 +0000

    Create square_root_and_fuXXX-math_version.c

---
+++ square_root_and_fuXXX-math_version.c
@@ -0,0 +1,45 @@
+//double for super cool precision
+double sqrt(double square)
+{
+    double root = square/3;
+    int i;
+    if (square <= 0) return 0;
+    for (i = 0; i<32; i += 1)
+    {
+        root = (root + square / root) / 2;
+    }
+    return root;
+
+}
+
+//oldf sqrt function
+void _sqrt(double square) //had to make it void so the compiler would stop yelling at me should just be a double
+{
+    double root = square/3;
+    int i;
+    //if (square <= 0) return 0;
+    //increment does not count, wouldn't work without it
+    for (i = 0; i<32; i += 1)
+    {
+        //i was told to censor this part
+    }
+    //wait i wasn't supposed to mention that wait shit
+}
+
+int main(int argc, char* argv[])
+{
+    //unfortunately only works with 1 digit numbers cause i'm lazy
+    if (strcmp(argv[1], "sqrt") == 0)
+    {
+        int number = (int)(argv[2][0] - '0');
+        printf("%lf", sqrt(number));
+        return 0;
+    }
+    else if (strcmp(argv[1], "-sqrt") == 0)
+    {
+        int number = (int)(argv[2][0] - '0');
+        //printf("%lf", _sqrt(number)); can't do this cause no return
+        return 0;
+    }
+    return 1;
+}
